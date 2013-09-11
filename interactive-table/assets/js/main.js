b2b18cb735acad8847dfb99e8fc33fd3cbf90985
Jennifer Patel <jpatel@inkyblakk.com>
2013-09-11 10:09:08 -0400

    interactive table - sorting fixed

--- interactive-table/assets/js/main.js
+++ interactive-table/assets/js/main.js
@@ -81,48 +81,51 @@ $(document).ready(function(){
 	$('thead th').each(function(column) {
 		//addClass .sortable and start click bind
 		//to allow certain columns to be sortable, remove the addClass(), change the selector to target thead th.sortable, or whatever you want to make the class
-		$(this).addClass('sortable').click(function(){
-			//get column that was clicked and use it to sort with
-			var findSortKey = function($cell) {
-				console.log ($cell.find('.sort-key').text().toUpperCase());
-				return $cell.find('.sort-key').text().toUpperCase() + ' ' + $cell.text().toUpperCase();
-			};
-			var sortDirection = $(this).is('.sorted-asc') ? -1 : 1;
+		//$('thead th.sortable').click(function(){
+		if($(this).hasClass('sortable')){
+			$(this).click(function(){
+				//get column that was clicked and use it to sort with
+				var findSortKey = function($cell) {
+					console.log ($cell.find('.sort-key').text().toUpperCase());
+					return $cell.find('.sort-key').text().toUpperCase() + ' ' + $cell.text().toUpperCase();
+				};
+				var sortDirection = $(this).is('.sorted-asc') ? -1 : 1;
 
-			//step back up the tree and get the rows with data for sorting
-			var $rows = $(this).parent().parent().parent().find('tbody tr').get();
+				//step back up the tree and get the rows with data for sorting
+				var $rows = $(this).parent().parent().parent().find('tbody tr').get();
 
-			//loop through all the rows and find   
-			$.each($rows, function(index, row) {
-				row.sortKey = findSortKey($(row).children('td').eq(column));
-			});
-			
-			//compare and sort the rows alphabetically
-			$rows.sort(function(a, b) {
-				if (a.sortKey < b.sortKey) return -sortDirection;
-				if (a.sortKey > b.sortKey) return sortDirection;
-					return 0;
-			});
-			
-			//add the rows in the correct order to the bottom of the table
-			$.each($rows, function(index, row) {
-				$('tbody').append(row);
-				row.sortKey = null;
-			});
-			
-			//identify the column sort order
-			$('th').removeClass('sorted-asc sorted-desc');
-			var $sortHead = $('th').filter(':nth-child(' + (column + 1) + ')');
-			sortDirection == 1 ? $sortHead.addClass('sorted-asc') : $sortHead.addClass('sorted-desc');
-			
-			//identify the column to be sorted by
-			$('td').removeClass('sorted')
-				.filter(':nth-child(' + (column + 1) + ')')
-				.addClass('sorted');
+				//loop through all the rows and find   
+				$.each($rows, function(index, row) {
+					row.sortKey = findSortKey($(row).children('td').eq(column));
+				});
 				
-			$('.visible td').removeClass('odd');
-			altRows('.visible:even td', 'odd');
-		});
+				//compare and sort the rows alphabetically
+				$rows.sort(function(a, b) {
+					if (a.sortKey < b.sortKey) return -sortDirection;
+					if (a.sortKey > b.sortKey) return sortDirection;
+						return 0;
+				});
+				
+				//add the rows in the correct order to the bottom of the table
+				$.each($rows, function(index, row) {
+					$('tbody').append(row);
+					row.sortKey = null;
+				});
+				
+				//identify the column sort order
+				$('th').removeClass('sorted-asc sorted-desc');
+				var $sortHead = $('th').filter(':nth-child(' + (column + 1) + ')');
+				sortDirection == 1 ? $sortHead.addClass('sorted-asc') : $sortHead.addClass('sorted-desc');
+				
+				//identify the column to be sorted by
+				$('td').removeClass('sorted')
+					.filter(':nth-child(' + (column + 1) + ')')
+					.addClass('sorted');
+					
+				$('.visible td').removeClass('odd');
+				altRows('.visible:even td', 'odd');
+			});
+		}
 	});
 });
 
