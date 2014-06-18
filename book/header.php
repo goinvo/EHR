c146cdc5355e67a04b128c3e285b7d16b69aea8b
Jennifer Patel <jpatel@inkyblakk.com>
2014-06-18 12:16:55 -0400

    Updated feedback email and headers
    Book
    - updated feedback email and header
    
    Interactive table
    - renamed interactive-table.html to index.html
    
    Timeline:
    - removed assets/js/ since timeline doesn't require jquery, and requires
    that d3 be in same directory
    - added timeline_frame changes to index.html
    - added patient picture to assets/images

--- book/header.php
+++ book/header.php
@@ -1,6 +1,6 @@
 <div class="container header">
 	<div class="sixteen columns">
-		<h2 class="title">EHR Style Guide <span class="version">v2, 3-24-2014</span></h2>
+		<h2 class="title">Inspired EHRs <span class="sm_title">Designing for Clinicians</span><!--<span class="version">v2, 3-24-2014</span>--></h2>
 		<div class="authors">
 			<p>Jeff Belden MD, Jennifer Patel, Richelle Koopman MD, Catherine Plaisant PhD, Joi Moore PhD, Nathan Lowrance, Shreeya Gangwal, Juhan Sonin</p>
 		</div>
@@ -11,10 +11,10 @@
 	<div class="container">
 		<div class="sixteen columns">
 			<ul>
-				<li><a href="https://www.dropbox.com/s/wsbnpwe5ioqunyh/EHR-02-16-sample.ibooks" target="blank">iBook</a></li>
-				<li><a href="https://github.com/goinvo/EHR" target="blank">Git Repo</a></li>
-				<li><a href="../index.html" target="blank">Interactives</a></li>
 				<li><a href="./index.html">Table of Contents</a></li>
+				<li><a href="../index.html" target="blank">Interactives</a></li>
+				<li><!--<a href="" target="blank">-->PDF<!--</a>--></li>
+				<li><a href="https://github.com/goinvo/EHR" target="blank">GitHub</a></li>
 				<li><a href="mailto:ehrstyle@gmail.com?Subject=EHR%20feedback" target="_top">Feedback</a></li>
 			</ul>
 		</div>
