bcc01593f3f42268175174154d44d253911f0b86
Jennifer Patel <jennifer@goinvo.com>
2014-06-27 15:53:50 -0400

    renamed book to designing for clinicians
    moved table of contents up one directory and added access to
    interactives in menu

---
+++ designing-for-clinicians/acknowledgements.php
@@ -0,0 +1,172 @@
+<?php $pageTitle='Acknowledgements'; ?>
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<title>Inspired EHRs | <?php echo $pageTitle; ?></title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="./assets/css/layout.css">
+	<link rel="stylesheet" type="text/css" media="print" href="./assets/css/print.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body class="acknowledgements">
+	<?php include './nav.php'; ?>
+	
+<div class="wrapper">
+	<?php include './header.php'; ?>
+				
+	<section id="acknowledgement-ch" class="chapter">
+		<header class="chapterStart">
+			<div class="container">
+				<div class="sixteen columns">
+					<h2>10</h2>
+					<h2>Authors &amp; Acknowledgements</h2>
+				</div>
+			</div>
+		</header>
+		<section class="section" id="authors">
+			<div class="container">
+				<div class="sixteen columns">
+					<div class="sectionStart">
+						<hr/>
+					</div>
+					<h3>About the Authors</h3>
+					<h5>Authors</h5>
+					<div class="author">
+						<p><strong>Jeff Belden MD</strong> s a family physician. He is currently a Professor of Clinical Family &amp; Community Medicine at the University of Missouri-Columbia School of Medicine, and he is also an affiliated faculty member with the Information Experience Laboratory. His current responsibilities include user training, EHR implementation, collaboration with human-computer interaction researchers from the Information Experience (IE) Lab, and participation in EHR innovation projects at the Tiger Institute, a technology collaborative between the University of Missouri and Cerner Corporation.</p>
+						<p>Dr. Belden’s research interests include the visual display of quantitative clinical information at the point of care, information display in clinical notes, and tools that foster collaborative conversations between patients and healthcare providers.</p>
+						<p>Dr. Belden was the Founding Chair of the HIMSS EHR Usability Task Force. He has given presentation on EHR usability at the HIMSS Annual Conference and Virtual Conferences, and at other national conferences. Dr. Belden’s past experience with photography, film-making, layout and design, typography, as well as his experience as a consultant in healthcare software design inform his approach to user-centered design.</p>
+						<p>He blogs on EHR usability at <a href="www.toomanyclicks.com" target="blank">www.toomanyclicks.com</a>.</p>
+					</div>
+					<div class="author">
+						<p><strong>Nathan Lowrance MA</strong> is a doctoral candidate in the School of Information Science and Learning Technology in the College of Education at the University of Missouri-Columbia. His research interests include human information seeking behavior, human-computer interaction design, and the psychology surrounding choices and biases. As a graduate assistant Lowrance worked on and led a variety of usability and health IT projects at the University of Missouri’s Information Experience Laboratory.</p>
+					</div>
+					<div class="author">
+						<p><strong>Jennifer Patel</strong> is an interface designer at Involution Studios in Boston. She has a fine arts degree in designing for new media from the Rochester Institute of Technology which has allowed her to specialize in interactive design and front-end development. Jennifer has designed complex systems and software for business-critical services from big data to streamlining processes, and has architected, designed, and built decision support tools for health and healthcare. She played a leading role in developing the designs and interactive prototypes found throughout the chapters, and was responsible for the book’s production.</p>
+					</div>
+					<div class="author">
+						<p><strong>Catherine Plaisant PhD</strong> is a Senior Research Scientist at the University of Maryland Institute for Advanced Computer Studies. In 1988, she joined Professor Ben Shneiderman at the Human-Computer Interaction Laboratory where she is now the Associate Director of the Research. She enjoys working with multidisciplinary teams on designing and evaluating new interface technologies that are both usable and useful. Catherine Plaisant has over 140 published papers, on subjects as diverse as information visualization, medical informatics, universal access, digital humanities, technology for families, and evaluation methodologies. With Ben Shneiderman she co-authored the 4th and 5th Editions of Designing the User Interface, one of the major books on Human-Computer Interaction.</p>
+					</div>
+
+					<div class="author">
+						<p><strong>Richelle Koopman MD, MS</strong> is an Associate Professor in the Department of Family and Community Medicine at the University of Missouri-Columbia School of Medicine. In addition to being a board-certified practicing Family Physician, Dr. Koopman has a master’s degree in Clinical Research from the Medical University of South Carolina. Dr. Koopman is experienced in conducting quantitative and qualitative research, and has also undertaken additional training regarding human factors and human-computer interaction. The Agency for Healthcare Research and Quality and the American Academy of Family Physicians Foundation have awarded her funding to investigate the EHR’s role in physician-patient interactions, including health information needs, information seeking by patients with chronic diseases and clinical decision support for care of patients with diabetes. She won the MU School of Medicine’s 2010 Dorsett L. Spurgeon MD Distinguished Medical Research Award.</p>
+					</div>
+
+					<div class="author">
+						<p><strong>Joi L. Moore, PhD</strong> is an Associate Division Director in the College of Education and an Associate Professor with the School of Information Science and Learning Technologies at the University of Missouri. In addition, she is a Core Faculty member in the MU Informatics Institute and an Affiliated Faculty in the Black Studies Department. Her research expertise includes designing user-centered web applications (human-computer interaction) and electronic performance support systems. She is a former President of the Training and Performance Division and the Minorities in Media Affiliate for the Association for Educational Communications and Technology (AECT). In addition, she serves on the faculty of the Information Experience Lab at the University of Missouri, which conducts usability testing projects for internal and external clients. Dr. Moore also brings her experience and expertise with user-centered design to the lab.</p>
+					</div>
+
+					<div class="author">
+						<p><strong>Todd R. Johnson, PhD</strong> is a professor of biomedical informatics at the University of Texas School of Biomedical Informatics (SBMI) at Houston. He has over 20 years of experience in biomedical informatics (BMI), and he is particularly interested in clinical informatics, user-centered design for health information technology, clinical data warehousing, and the secondary use of clinical care data. With Drs. Ben Shneiderman and Catherine Plaisant of the University of Maryland he co-led the ONC-funded SHARP-C subproject to develop novel interactive visualizations that help researchers and clinicians explore and understand clinical data in ways that enhance the efficiency, quality, and safety of EHRs. He also coordinated SHARPC’s development of Safety Enhanced Design Guidelines for user-centered cognitive design of health IT. </p>
+					</div>
+
+					<div class="author">
+						<p><strong>Juhan Sonin</strong> is the Creative Director of Involution Studios Boston. He is responsible for all product design and service delivery, contributes to the business management of Boston operations and participates in corporate Involution planning. Since 2008, Juhan has been lecturing at MIT on design and rapid prototyping. His experience includes design and engineering management, interaction design and user experience. His extensive human-computer interaction (HCI) expertise led to numerous award-winning, on-the-shelf products and applications. He has held positions at Apple, NCSA, MIT, MITRE and several startups.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="section" id="acknowledgements-sect">
+			<div class="container">
+				<div class="sixteen columns">
+					<div class="sectionStart">
+						<hr/>
+					</div>
+					<h3>Acknowledgements</h3>
+					<p>We are grateful to the many people and organizations that provided support, feedback, and general encouragement for this project.</p>
+					<p>We received feedback and suggestions from an advisory panel and many other reviewers.  Academic researchers contributed their expertise in design, and their understanding of the human factors involved and current research. The advisory panel also included clinicians who are active users of EHRs and other health IT products. These users brought their keen personal experience and expertise to bear on the project.</p>
+					<p>Several members of the Electronic Health Records Association (EHRA) and other HealthIT professionals participated in the three design workshops we organized during the course of the project. Others read early drafts of the book and provided feedback and suggestions.</p>
+					<p>Finally we thank the California HealthCare Foundation and the SHARP-C Project of the Office of the National Coordinator for Health IT for sponsoring this project.</p>
+					
+					<h5>Advisory Panel</h5>
+						<ul class="disc">
+							<li>John Beasley, MD - Professor of Medicine and Industrial &amp; Systems Engineering at the University of Wisconsin-Madison</li>
+							<li>Elmer V. Bernstam, MD, MSE - Professor and Associate Dean for Research, School of Biomedical Informatics; Professor of Internal Medicine, Medical School, The University of Texas Health Sciences Center at Houston</li>
+							<li>Craig Bradley, MD - Chief Medical Informatics Officer, Texas Tech University and Health Sciences Center, Lubbock, TX</li>
+							<li>David Brick, MD - Pediatric cardiologist, Village Pediatric Cardiology, New York</li>
+							<li>Jason Cafer, MD - Psychiatrist, Health IT data visualization &amp; software entrepreneur in Columbia, MO</li>
+							<li>Frank Caligiuri, PharmD - Clinical Assistant Professor, Drake University College of Pharmacy and Health Sciences, Des Moines, IA</li>
+							<li>Deborah Cohen, PhD - Associate Professor in the Department of Family Medicine at Oregon Health &amp; Science University</li>
+							<li>Ann Schoofs Hundt, PhD - Industrial Engineer, University of Wisconsin-Wisconsin Center for Quality and Productivity Improvement</li>
+							<li>Eleanor Hunt, MSN, RN, BC - Healthcare Informatics Consultant, Raleigh, NC</li>
+							<li>Robert Pierce, MD - Family Physician Fulton, MO, former Medical Director for PPS-McKesson, currently Medical Director at Data Strategies</li>
+							<li>Ben Shneiderman, PhD - Professor of Computer Science at the University of Maryland, and founding director of the Human-Computer Interaction Lab</li>
+							<li>Linsey Steege, PhD - Human Factors Engineer, Assistant Professor,  School of Nursing at the University of Wisconsin-Madison</li>
+						</ul>
+
+					<h5>Health IT Vendors</h5>
+					<p><strong>Workshop Participants</strong></p>
+						<ul>
+							<li>LesLea Carey - Clinical UI Analyst, NextGen Healthcare</li>
+							<li>Roy Gill, MD - Physician Director of Clinical Content, NextGen Healthcare</li>
+							<li>Marjorie Otterson - Director, User Experience Design, athenahealth</li>
+							<li>Frances Wu - Senior Strategist - EMR Strategy, Cerner</li>
+							<li>Seth Claybrook - Interaction Designer, Cerner</li>
+							<li>Philip Rawson - Designer, MedSocket</li>
+							<li>Karl Kochendorfer, MD - President, MedSocket; Assistant Vice President of Health Affairs & Chief Health Information Officer, University of Illinois Hospital and Health Sciences System</li>
+							<li>Robert Pierce, MD - Family Physician Fulton, MO, former Medical Director for PPS-McKesson, currently Medical Director at Data Strategies</li>
+							<li>Art Swanson - Director of User Experience, Allscripts</li>
+							<li>Gary Gartner, MD - Principal UX Clinician, Allscripts</li>
+						</ul>
+   					<p><strong>Reviewers</strong></p>
+   						<ul>
+							<li>Becca Green, PhD - Usability Research Lead, Cerner</li>
+							<li>Janey Barnes, PhD - Human Factors Specialist, User-View, Inc.</li>
+							<li>Gary Gartner, MD, MS - Principal UX Clinician, Allscripts</li>
+							<li>Lynda Hoeksema, MSN, FNP-BC, RN-BC - Clinical Transformation Manager - Spectrum Health</li>
+						</ul>
+
+					<h5>Additional Acknowledgements</h5>
+					<p>The authors wish to thank the following people for their contribution to this book.</p>
+						<ul>
+							<li><strong>Erin Horak, editor</strong></li>
+							<li><strong>Shreeya Gangwal</strong> and <strong>Adam Pere</strong>, of Involution Studios, participated in the development of the software prototypes used in this book.</li>
+							<li><strong>Andrei Herasimchuk</strong> and <strong>Donna Driscoll</strong>, of Involution Studios, contributed to the Design Principles chapter.</li>
+							<li><strong>Tiffany Chao</strong> and <strong>Johnny Wu</strong>, graduate students at the University of Maryland,  developed the Twinlist prototype, under the supervision of Catherine Plaisant and Ben Shneiderman.</li>
+						</ul>
+
+					<h5>Electronic Health Records Association</h5>
+					<p>We are deeply grateful for the Electronic Health Record Association (EHRA)’s support and encouragement for this project. Although the EHRA’s participation in this project does not constitute their endorsement of the content of the book, we appreciate that throughout the project, the EHRA has engaged with our work and provided feedback. In addition, the EHRA provided our authors with opportunities to present our project’s progress. Their volunteer participants also supported us by contributing to our design workshop and dissemination efforts.</p>
+					
+					<h5>Funding Sponsors</h5>
+					<p><a href="http://www.chcf.org/">California HealthCare Foundation</a><br/>
+						The California HealthCare Foundation, based in Oakland, California, works as a catalyst to fulfill the promise of better healthcare for all Californians. The Foundation supports ideas and innovations that improve the quality, increase the efficiency, and lower the costs of care. For more information, visit <a href="www.chcf.org">www.chcf.org</a>.</p>
+
+					<p><a href="https://sbmi.uth.edu/nccd/index.htm">SHARP-C Project of the Office of the National Coordinator for Health IT</a> <br/>
+						This project was supported by the Office of the National Coordinator for Health Information Technology, Grant No. 10510592 for Patient-Centered Cognitive Support, under the Strategic Health IT Advanced Research Projects (SHARP).</p>
+				
+				</div>
+			</div>
+		</section>
+
+		<footer class="chapterEnd">
+			<div class="container">
+				<div class="sixteen columns">
+					<div class="sectionStart">
+						<hr />
+					</div>
+					<div class="cf">
+						<div class="fleft" id="prevCh">
+							<p class="pagination prevCh"><a href="./design-principles.php"><span class="prev"></span>Design Principles</a></p></div>		
+					</div>
+				</div>
+			</div>
+		</footer>
+	</section>
+	
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script src="./assets/js/main.js" type="text/javascript"></script>
+
+</div><!--end wrapper-->
+</body>
+</html>
---
+++ designing-for-clinicians/header.php
@@ -0,0 +1,21 @@
+<div class="container header">
+	<div class="sixteen columns">
+		<h2 class="title">Inspired EHRs: <span class="sm_title">Designing for Clinicians</span><!--<span class="version">v2, 3-24-2014</span>--></h2>
+	</div>
+</div>
+	
+<div class="menu">
+	<div class="container">
+		<div class="sixteen columns">
+			<ul>
+				<li><a href="../">Table of Contents</a></li>
+				<li><a href="../simple-list/">Simple List</a></li>
+				<li><a href="../medication-list/">Interacive Table</a></li>
+				<li><a href="../timeline/">Timeline</a></li>
+				<li><a href="./Inspired_EHRs.pdf" target="blank">PDF</a></li>
+				<li><a href="https://github.com/goinvo/EHR" target="blank">GitHub</a></li>
+				<li><a href="mailto:feedback@inspiredEHRs.com?Subject=EHR%20feedback" target="_top">Feedback</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
