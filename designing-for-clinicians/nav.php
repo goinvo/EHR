c16e0206d33932f4b311d6acefe696ab68b0c599
Jennifer Patel <jennifer@goinvo.com>
2014-06-27 16:56:30 -0400

    restyled chapter nav to always be on top

--- designing-for-clinicians/nav.php
+++ designing-for-clinicians/nav.php
@@ -1,71 +1,52 @@
 <nav class="main">
+	<div class="container">
+	<div class="sixteen columns">
 	<ul>
-		<li><a href="./index.php"><span class="ch-num"> &nbsp;</span> <span class="ch-name">Table of Contents</span></a></li>
-		<li class="foreword"><a href="./foreword.php"><span class="ch-num"> 0</span> <span class="ch-name">Foreword</span></a> <a href="./about-this-book.php"><span class="next"></span></a></li>
-		<li class="about-this-book"><a href="./foreword.php"><span class="prev"></span></a> <a href="./about-this-book.php"><span class="ch-num"> 1</span> <span class="ch-name">About this Book</span></a> <a href="./medication-list.php"><span class="next"></span></a></li>
-		<li class="medication-list"><a href="./about-this-book.php"><span class="prev"></span></a> <a href="./medication-list.php"><span class="ch-num"> 2</span> <span class="ch-name">Medication List</span></a> <a href="./medication-reconciliation.php"><span class="next"></span></a>
-			<ul>
-				<li><a href="./medication-list.php#medication-lists"><span class="ch-num">2.1</span> <span class="ch-name">Medication Lists</span></a></li>
-				<li><a href="./medication-list.php#situational-awareness"><span class="ch-num">2.2</span> <span class="ch-name">Situational Awareness</span></a></li>
-				<li><a href="./medication-list.php#simple-list"><span class="ch-num">2.3</span> <span class="ch-name">Simple List</span></a></li>
-				<li><a href="./medication-list.php#interactive-table"><span class="ch-num">2.4</span> <span class="ch-name">Interactive Table</span></a></li>
-				<li><a href="./medication-list.php#timeline"><span class="ch-num">2.5</span> <span class="ch-name">Medication Timeline</span></a></li>
-			</ul>
-		</li>
-		<li class="medication-reconciliation"><a href="./medication-list.php"><span class="prev"></span></a> <a href="./medication-reconciliation.php"><span class="ch-num"> 3</span> <span class="ch-name">Medication Reconciliation</span></a> <a href="./allergy-list.php"><span class="next"></span></a>
-			<ul>
-				<li><a href="./medication-reconciliation.php#medication-reconciliation"><span class="ch-num">3.1</span> <span class="ch-name">Medication Reconciliation</span></a></li>
-				<li><a href="./medication-reconciliation.php#inpatient"><span class="ch-num">3.2</span> <span class="ch-name">Inpatient</span></a></li>
-				<li><a href="./medication-reconciliation.php#ambulatory"><span class="ch-num">3.3</span> <span class="ch-name">Ambulatory</span></a></li>
-				<li><a href="./medication-reconciliation.php#review"><span class="ch-num">3.4</span> <span class="ch-name">Review</span></a></li>
-			</ul>
-		</li>
-		<li class="allergy-list"><a href="./medication-reconciliation.php"><span class="prev"></span></a> <a href="./allergy-list.php"><span class="ch-num"> 4</span> <span class="ch-name">Allergy List</span></a> <a href="./eprescribing.php"><span class="next"></span></a>
-			<ul>
-				<li><a href="./allergy-list.php#allergy-list"><span class="ch-num">4.1</span> <span sclass="ch-name">Allergy List</span></a></li>
-				<li><a href="./allergy-list.php#big-three"><span class="ch-num">4.2</span> <span class="ch-name">Big Three</span></a></li>
-				<li><a href="./allergy-list.php#glancing-at-the-allergy-list"><span class="ch-num">4.3</span> <span class="ch-name"> Glancing at the Allergy List</span></a></li>
-				<li><a href="./allergy-list.php#exploring-the-details"><span class="ch-num">4.4</span> <span class="ch-name">Exploring the Details</span></a></li>
-				<li><a href="./allergy-list.php#changing-the-allergy-list"><span class="ch-num">4.5</span> <span class="ch-name">Changing the Allergy List</span></a></li>
-				<li><a href="./allergy-list.php#review"><span class="ch-num">4.6</span> <span class="ch-name">Review</span></a></li>
-			</ul>
-		</li>
-		<li class="eprescribing"><a href="./allergy-list.php"><span class="prev"></span></a> <a href="./eprescribing.php"><span class="ch-num"> 5</span> <span class="ch-name">ePrescribing</span></a> <a href="./drug-alerts.php"><span class="next"></span></a>
-			<ul>
-				<li><a href="./eprescribing.php#eprescribing"><span class="ch-num">5.1</span> <span class="ch-name">Drug Allergy Alerts</span></a></li>
-				<li><a href="./eprescribing.php#cpoe"><span class="ch-num">5.2</span> <span class="ch-name">Drug Interaction Alerts</span></a></li>
-				<li><a href="./eprescribing.php#summary"><span class="ch-num">5.3</span> <span class="ch-name">Summary</span></a></li>
-			</ul>
-		</li>
-		<li class="drug-alerts"><a href="./eprescribing.php"><span class="prev"></span></a> <a href="./drug-alerts.php"><span class="ch-num"> 6</span> <span class="ch-name">Drug Alerts</span></a> <a href="./human-factors.php"><span class="next"></span></a>
-			<ul>
-				<li><a href="./drug-alerts.php#drug-allergy-alerts"><span class="ch-num">6.1</span> <span class="ch-name">Drug Allergy Alerts</span></a></li>
-				<li><a href="./drug-alerts.php#drug-interaction-alerts"><span class="ch-num">6.2</span> <span class="ch-name">Drug Interaction Alerts</span></a></li>
-				<li><a href="./drug-alerts.php#clinical-decision-support"><span class="ch-num">6.3</span> <span class="ch-name">Clinical Decision Support</span></a></li>
-				<li><a href="./drug-alerts.php#summary"><span class="ch-num">6.4</span> <span class="ch-name">Summary</span></a></li>
-			</ul>
-		</li>
-		<li class="human-factors"><a href="./drug-alerts.php"><span class="prev"></span></a> <a href="./human-factors.php"><span class="ch-num"> 7</span> <span class="ch-name">Human Factors</span></a> <a href="./design-principles.php"><span class="next"></span></a>
-			<ul>
-				<li><a href="./human-factors.php#how-people-perceive"><span class="ch-num">7.1</span> <span class="ch-name">How People Perceive</span></a></li>
-				<li><a href="./human-factors.php#how-people-think"><span class="ch-num">7.2</span> <span class="ch-name">How People Think</span></a></li>
-				<li><a href="./human-factors.php#working-memory"><span class="ch-num">7.3</span> <span class="ch-name">Working Memory</span></a></li>
-				<li><a href="./human-factors.php#cognitive-load"><span class="ch-num">7.4</span> <span class="ch-name">Cognitive Load</span></a></li>
-			</ul>
-		</li>
-		<li class="design-principles"><a href="./human-factors.php"><span class="prev"></span></a> <a href="./design-principles.php"><span class="ch-num"> 8</span> <span class="ch-name">Design Principles</span></a> <a href="./health-it-usability.php"><span class="next"></span></a>
-			<ul>
-				<li><a href="./design-principles.php#mental-model"><span class="ch-num">8.1</span> <span class="ch-name">Mental Model</span></a></li>
-				<li><a href="./design-principles.php#removing-complexity"><span class="ch-num">8.2</span> <span class="ch-name">Removing Complexity</span></a></li>
-				<!--<li><a href="#direct-manipulation"><div class="disc">8.3</div></a></li>-->
-				<li><a href="./design-principles.php#terminology"><span class="ch-num">8.3</span> <span class="ch-name">Terminology</span></a></li>
-				<li><a href="./design-principles.php#emphasis"><span class="ch-num">8.4</span> <span class="ch-name">Emphasis</span></a></li>
-				<li><a href="./design-principles.php#typography"><span class="ch-num">8.5</span> <span class="ch-name">Typography</span></a></li>
-				<li><a href="./design-principles.php#color"><span class="ch-num">8.6</span> <span class="ch-name">Color</span></a></li>
-				<!--<li><a href="#visual-clutter"><div class="disc">8.8</div></a></li>-->
-			</ul>
-		</li>
-		<li class="health-it-usability"><a href="./design-principles.php"><span class="prev"></span></a> <a href="./health-it-usability.php"> <span class="ch-num"> 9</span> <span class="ch-name">Health IT Usability</span></a> <a href="./acknowledgements.php"><span class="next"></span></li>
-		<li class="acknowledgements"><a href="./health-it-usability.php"><span class="prev"></span></a> <a href="./acknowledgements.php"> <span class="ch-num"> 9</span> <span class="ch-name">Acknowledgements</span></a> </li>
+		<li class="foreword">
+			<a href="../"><span class="prev"></span></a>
+			<a href="./foreword.php"><span class="ch-num"> 0</span> <span class="ch-name">Foreword</span></a> 
+			<a href="./about-this-book.php"><span class="next"></span></a></li>
+		<li class="about-this-book">
+			<a href="./foreword.php"><span class="prev"></span></a> 
+			<a href="./about-this-book.php"><span class="ch-num"> 1</span> <span class="ch-name">About this Book</span></a> 
+			<a href="./medication-list.php"><span class="next"></span></a></li>
+		<li class="medication-list">
+			<a href="./about-this-book.php"><span class="prev"></span></a> 
+			<a href="./medication-list.php"><span class="ch-num"> 2</span> <span class="ch-name">Medication List</span></a> 
+			<a href="./medication-reconciliation.php"><span class="next"></span></a></li>
+		<li class="medication-reconciliation">
+			<a href="./medication-list.php"><span class="prev"></span></a> 
+			<a href="./medication-reconciliation.php"><span class="ch-num"> 3</span> <span class="ch-name">Medication Reconciliation</span></a> 
+			<a href="./allergy-list.php"><span class="next"></span></a></li>
+		<li class="allergy-list">
+			<a href="./medication-reconciliation.php"><span class="prev"></span></a> 
+			<a href="./allergy-list.php"><span class="ch-num"> 4</span> <span class="ch-name">Allergy List</span></a> 
+			<a href="./eprescribing.php"><span class="next"></span></a></li>
+		<li class="eprescribing">
+			<a href="./allergy-list.php"><span class="prev"></span></a> 
+			<a href="./eprescribing.php"><span class="ch-num"> 5</span> <span class="ch-name">ePrescribing</span></a> 
+			<a href="./drug-alerts.php"><span class="next"></span></a></li>
+		<li class="drug-alerts">
+			<a href="./eprescribing.php"><span class="prev"></span></a> 
+			<a href="./drug-alerts.php"><span class="ch-num"> 6</span> <span class="ch-name">Drug Alerts</span></a> 
+			<a href="./human-factors.php"><span class="next"></span></a></li>
+		<li class="human-factors">
+			<a href="./drug-alerts.php"><span class="prev"></span></a> 
+			<a href="./human-factors.php"><span class="ch-num"> 7</span> <span class="ch-name">Human Factors</span></a> 
+			<a href="./design-principles.php"><span class="next"></span></a></li>
+		<li class="design-principles">
+			<a href="./human-factors.php"><span class="prev"></span></a> 
+			<a href="./design-principles.php"><span class="ch-num"> 8</span> <span class="ch-name">Design Principles</span></a> 
+			<a href="./health-it-usability.php"><span class="next"></span></a></li>
+		<li class="health-it-usability">
+			<a href="./design-principles.php"><span class="prev"></span></a> 
+			<a href="./health-it-usability.php"> <span class="ch-num"> 9</span> <span class="ch-name">Health IT Usability</span></a> 
+			<a href="./acknowledgements.php"><span class="next"></span></a></li>
+		<li class="acknowledgements">
+			<a href="./health-it-usability.php"><span class="prev"></span></a>
+			<a href="./acknowledgements.php"> <span class="ch-num"> 9</span> <span class="ch-name">Acknowledgements</span></a> 
+			<a href="../"><span class="next"></span></a></li>
 	</ul>
+	</div>
+	</div>
 </nav>
